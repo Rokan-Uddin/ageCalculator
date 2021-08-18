b30ac78c5436b5a06a608b378a93bfb1d5e3039f
Rokan Uddin <rokanu55@gmail.com>
2021-08-18 22:40:14 +0600

    responsive & add style

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const wellcomeTextElement = document.createElement("h1"); 
 wellcomeTextElement.innerHTML = "";
-wellcomeTextElement.style.textAlign="center";
 wellcomeTextElement.setAttribute("id","userText");                  
 document.body.appendChild(wellcomeTextElement);
 
 const ageTextElement=document.createElement("h1");
 ageTextElement.innerText="";
-ageTextElement.style.textAlign="center";
 ageTextElement.setAttribute("id","agetext");
 document.body.appendChild(ageTextElement);
 
+const videoElement=document.createElement("video");
+videoElement.setAttribute("id","video");
+videoElement.setAttribute("autoplay","true");
+document.body.appendChild(videoElement);
+
+const canvasElement=document.createElement("canvas");
+canvasElement.setAttribute("width","300");
+canvasElement.setAttribute("height","300");
+canvasElement.setAttribute("id","canvas");
+canvasElement.style.display="none";
+document.body.appendChild(canvasElement);
+
+const imageElement = document.createElement("img");
+imageElement.setAttribute("id","img");
+document.body.appendChild(imageElement);
+
 const refreshButton=document.createElement("button");
 refreshButton.innerText="refresh";
-refreshButton.style.textAlign="center";
 refreshButton.setAttribute("id","refreshbutton");
 document.body.appendChild(refreshButton);
+
+const captureButton=document.createElement("button");
+captureButton.innerText="Capture";
+captureButton.setAttribute("id","capturebutton");
+document.body.appendChild(captureButton);
+
+let canvas =document.getElementById("canvas");
+var context = canvas.getContext("2d");
+let video = document.getElementById("video");
 
 function setUser() {
     if(localStorage.getItem("userName")===null) {
@@ -36,6 +58,44 @@ function setUser() {
         }
     }
 }
+function runVideo() {
+    if(navigator.mediaDevices.getUserMedia && !localStorage.getItem("image")) {
+        navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
+            video.srcObject=stream;
+            video.play();
+        });
+    }
+    else {
+        document.getElementById("video").style.display="none";
+        document.getElementById("capturebutton").style.display="none";
+    }
+}
+function getImage() {
+    var dataURL = localStorage.getItem("image");
+    document.getElementById("img").src=dataURL;
+}
+runVideo();
+getImage();
+
+document.getElementById("refreshbutton").addEventListener("click",()=>{
+    document.getElementById("userText").innerText="";
+    document.getElementById("agetext").innerText="";
+    document.getElementById("img").setAttribute("src","");
+    document.getElementById("video").style.display="block";
+    document.getElementById("capturebutton").style.display="inline";
+    document.getElementById("refreshbutton").style.display="none";
+    localStorage.clear();
+    runVideo();
+})
+
+document.getElementById("capturebutton").addEventListener("click", ()=>{
+    context.drawImage(video,0,0,300,300);
+    localStorage.setItem("image", canvas.toDataURL());
+    document.getElementById("video").style.display="none";
+    document.getElementById("capturebutton").style.display="none";
+    document.getElementById("refreshbutton").style.display="block";
+    getImage();
+});
 setInterval(() => {
     setUser();
     const currentDate=new Date();
@@ -58,67 +118,3 @@ setInterval(() => {
     }
 
 }, 1000);
-
-document.getElementById("refreshbutton").addEventListener("click",()=>{
-    document.getElementById("userText").innerText="";
-    document.getElementById("agetext").innerText="";
-    document.getElementById("img").setAttribute("src","");
-    document.getElementById("video").style.display="block";
-    localStorage.clear();
-    runVideo();
-})
-
-//Assignment -3
-const videoElement=document.createElement("video");
-videoElement.setAttribute("width","640");
-videoElement.setAttribute("height","480");
-videoElement.setAttribute("autoplay","true");
-videoElement.setAttribute("id","video");
-document.body.appendChild(videoElement);
-
-const canvasElement=document.createElement("canvas");
-canvasElement.setAttribute("width","300")
-canvasElement.setAttribute("height","300")
-canvasElement.setAttribute("id","canvas");
-canvasElement.style.display="none";
-document.body.appendChild(canvasElement);
-
-const imageElement = document.createElement("img");
-imageElement.setAttribute("id","img");
-document.body.appendChild(imageElement);
-
-const captureButton=document.createElement("button");
-captureButton.innerText="Capture";
-captureButton.style.textAlign="center";
-captureButton.setAttribute("id","capturebutton");
-document.body.appendChild(captureButton);
-
-let canvas =document.getElementById("canvas");
-var context = canvas.getContext("2d");
-let video = document.getElementById("video");
-function runVideo() {
-    if(navigator.mediaDevices.getUserMedia && !localStorage.getItem("image")) {
-        navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
-            video.srcObject=stream;
-            video.play();
-        });
-    }
-    else {
-        document.getElementById("video").style.display="none";
-        document.getElementById("capturebutton").style.display="none";
-    }
-}
-runVideo();
-getImage();
-document.getElementById("capturebutton").addEventListener("click", ()=>{
-    context.drawImage(video,0,0,300,300);
-    localStorage.setItem("image", canvas.toDataURL());
-    document.getElementById("video").style.display="none";
-    document.getElementById("capturebutton").style.display="none";
-    getImage();
-});
-
-function getImage() {
-        var dataURL = localStorage.getItem("image");
-        document.getElementById("img").src=dataURL;
-}
